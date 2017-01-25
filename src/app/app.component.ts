a8c511f706a04087f8e82ce3febf882b7366f3bb
Carlos Freitas <scarlosfreitas@gmail.com>
2017-01-24 22:30:36 -0200

    Primeiro Menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,20 +3,33 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
+import { MenuTestPage } from '../pages/menu-test/menu-test';
 
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
+  pages: Array<{component: any, title: string, icon: string}>;
   rootPage = HomePage;
 
   constructor(platform: Platform) {
+
+    this.pages = [
+      {component: HomePage,title: 'Home Page',icon: 'home'},
+      {component: MenuTestPage,title: 'Home Test',icon: 'menu'}
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
+  }
+
+  openPage(page) : void {
+    this.rootPage = page.component;
   }
 }
